71b2d9b4964744f30dc95f8ab0501b6bda209893
Michael S. Molina <70410625+michael-s-molina@users.noreply.github.com>
2023-04-06 09:36:34 -0300

    feat: Enables ECharts legend selector (#23590)
    (cherry picked from commit 30f210b84242051fe2f3d812ad8f5a10a0bed1b5)

--- superset-frontend/plugins/plugin-chart-echarts/src/utils/series.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/utils/series.ts
@@ -28,6 +28,7 @@ import {
   NumberFormatter,
   TimeFormatter,
   AxisType,
+  SupersetTheme,
 } from '@superset-ui/core';
 import { format, LegendComponentOption, SeriesOption } from 'echarts';
 import {
@@ -231,6 +232,7 @@ export function getLegendProps(
   type: LegendType,
   orientation: LegendOrientation,
   show: boolean,
+  theme: SupersetTheme,
   zoomable = false,
 ): LegendComponentOption | LegendComponentOption[] {
   const legend: LegendComponentOption | LegendComponentOption[] = {
@@ -241,6 +243,13 @@ export function getLegendProps(
       : 'vertical',
     show,
     type,
+    selector: ['all', 'inverse'],
+    selectorLabel: {
+      fontFamily: theme.typography.families.sansSerif,
+      fontSize: theme.typography.sizes.s,
+      color: theme.colors.grayscale.base,
+      borderColor: theme.colors.grayscale.base,
+    },
   };
   switch (orientation) {
     case LegendOrientation.Left:
