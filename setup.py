8207813c32b259218ef12b8792ce5d4a74be566f
John Bodley <4567245+john-bodley@users.noreply.github.com>
2023-05-16 06:54:46 -0700

    chore(trino): Bump Trino version to address an IANA time zone issue (#24069)
    (cherry picked from commit 58e1bcde9e1643cb4f2b3855a586305e75e23f84)

--- setup.py
+++ setup.py
@@ -167,7 +167,7 @@ setup(
         "pinot": ["pinotdb>=0.3.3, <0.4"],
         "postgres": ["psycopg2-binary==2.9.5"],
         "presto": ["pyhive[presto]>=0.6.5"],
-        "trino": ["trino>=0.319.0"],
+        "trino": ["trino>=0.324.0"],
         "prophet": ["prophet>=1.0.1, <1.1", "pystan<3.0"],
         "redshift": ["sqlalchemy-redshift>=0.8.1, < 0.9"],
         "rockset": ["rockset>=0.8.10, <0.9"],
