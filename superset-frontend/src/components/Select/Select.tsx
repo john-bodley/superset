8b88480c6390e9abc60189564d41405d43054109
JUST.in DO IT <justin.park@airbnb.com>
2023-05-22 10:32:45 -0700

    fix(sqllab): Table options rendering regression (#24142)
    (cherry picked from commit 6a44e0ddd4a48f384bd01143dc3e11ddf2ebce24)

--- superset-frontend/src/components/Select/Select.tsx
+++ superset-frontend/src/components/Select/Select.tsx
@@ -47,6 +47,7 @@ import {
   selectAllOption,
   mapValues,
   mapOptions,
+  hasCustomLabels,
 } from './utils';
 import { SelectOptionsType, SelectProps } from './types';
 import {
@@ -440,6 +441,11 @@ const Select = forwardRef(
       onChange?.(newValues, newOptions);
     };
 
+    const shouldRenderChildrenOptions = useMemo(
+      () => selectAllEnabled || hasCustomLabels(options),
+      [selectAllEnabled, options],
+    );
+
     const customMaxTagPlaceholder = () => {
       const num_selected = ensureIsArray(selectValue).length;
       const num_shown = maxTagCount as number;
@@ -492,6 +498,7 @@ const Select = forwardRef(
               <StyledCheckOutlined iconSize="m" aria-label="check" />
             )
           }
+          {...(!shouldRenderChildrenOptions && { options: fullSelectOptions })}
           oneLine={oneLine}
           tagRender={customTagRender}
           {...props}
@@ -507,7 +514,8 @@ const Select = forwardRef(
               {selectAllLabel()}
             </Option>
           )}
-          {renderSelectOptions(fullSelectOptions)}
+          {shouldRenderChildrenOptions &&
+            renderSelectOptions(fullSelectOptions)}
         </StyledSelect>
       </StyledContainer>
     );
